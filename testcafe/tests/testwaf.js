b08bffed68fbb6bcd39cd233209c8792d6c4ed85
franbuehler <franziska.buehler.schmocker@gmail.com>
2020-02-09 22:34:18 +0100

    Add AWS builspec

--- testcafe/tests/testwaf.js
+++ testcafe/tests/testwaf.js
@@ -7,6 +7,6 @@ fixture`Getting Started`
 test('WAF Test with malicious string', async t => {
     await t
         .typeText('input#email', 'test@test>')
-        .typeText('input#password', '<script>alert("My evil WAF Test")</script>')
+        .typeText('input#password', '<script>alert("My evil WAF Test");</script>')
         .click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
 });
